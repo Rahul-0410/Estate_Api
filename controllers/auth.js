cdb2de2a57ad41689d98caf43b16f4a7372e0775
Rahul-0410 <rgs786999@gmail.com>
2024-11-01 23:29:27 +0530

    first

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 import bcrypt from "bcrypt";
+import prisma from "../lib/prisma.js";
 
 export const register= async(req,res)=>{
     const {username,email,password} = req.body;
@@ -6,8 +7,18 @@ export const register= async(req,res)=>{
     const hashed=await  bcrypt.hash(password,10)
 
     console.log(hashed);
-    
-    
+
+    const newUser= await prisma.user.create({
+        data: {
+            username,
+            email,
+            password: hashed
+        },
+    });
+
+    console.log(newUser);
+
+    res.status(201).json({message:"User created successfully"});
 }
 export const login= (req,res)=>{
 
