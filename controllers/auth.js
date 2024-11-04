110ac045b136bc9c5a98b77b86abf07a8f57348d
Rahul-0410 <rgs786999@gmail.com>
2024-11-04 20:48:46 +0530

    2

--- controllers/auth.js
+++ controllers/auth.js
@@ -61,12 +61,13 @@ export const login= async(req,res)=>{
             id:user.id
         },process.env.JWT_SECRET_KEY,{expiresIn: age})
 
+        const {password: userPassword, ...userInfo}= user;
         res.cookie("token",token,{
             httpOnly:true,
             //remeber to uncomment while in production
             // secure:true
             maxAge:age
-        }).status(200).json({message:"Login Successful!"});
+        }).status(200).json(userInfo);
 
 
     }catch(err){
