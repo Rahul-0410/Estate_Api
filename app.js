f035c4f72555f67e55f104a2a633bbd5b537926d
Rahul-0410 <rgs786999@gmail.com>
2024-11-05 20:38:46 +0530

    done

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import postRoute from "./routes/post.route.js"
 import authRoute from "./routes/auth.route.js"
+import testRoute from "./routes/test.route.js"
 
 const app=express();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cookieParser())
 app.use("/api/posts",postRoute);
 app.use("/api/auth",authRoute);
+app.use("/api/test",testRoute);
 
 // nodemon --env-file .env .\app.js
 
